523833ed82200240dbe2e0677b9ce30d721f25b9
djfdmator <djfdmator@gmail.com>
2019-06-15 17:01:02 +0900

    yeaah
    move to desktop

---
+++ SLIM(e)OP/Assets/New/Assets 1/CameraMover.cs
@@ -0,0 +1,59 @@
+﻿//-----------------------------
+using UnityEngine;
+using System.Collections;
+//-----------------------------
+public class CameraMover : MonoBehaviour 
+{
+	//-----------------------------
+	//Total time for animation
+	public float TotalTime = 5.0f;
+
+	//Total Distance to move on each axis
+	public float TotalDistance = 30.0f;
+
+	//Curves for motion
+	public AnimationCurve XCurve;
+	public AnimationCurve YCurve;
+	public AnimationCurve ZCurve;
+
+	//Transform for this object
+	private Transform ThisTransform = null;
+	//-----------------------------
+	void Start()
+	{
+		//Get transform component
+		ThisTransform = GetComponent<Transform>();
+
+		//Start animation
+		StartCoroutine(PlayAnim());
+	}
+	//-----------------------------
+	public IEnumerator PlayAnim()
+	{
+		//Time that has passed since anim start
+		float TimeElapsed = 0.0f;
+
+		while(TimeElapsed < TotalTime)
+		{
+			//Get normalized time
+			float NormalTime = TimeElapsed / TotalTime;
+
+			//Sample graph for X Y and Z
+			Vector3 NewPos = ThisTransform.right.normalized * XCurve.Evaluate(NormalTime) * TotalDistance;
+			NewPos += ThisTransform.up.normalized * YCurve.Evaluate(NormalTime) * TotalDistance;
+			NewPos += ThisTransform.forward.normalized * ZCurve.Evaluate(NormalTime) * TotalDistance;
+
+			//Update position
+			ThisTransform.position = NewPos;
+
+			//Wait until next frame
+			yield return null;
+
+			//Update time
+			TimeElapsed += Time.deltaTime;
+		}
+	}
+	//-----------------------------
+
+}
+//-----------------------------
---
+++ SLIM(e)OP/Assets/Script/PlayerController.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State_Slime
+{
+    NONE,
+    IDLE,
+    MOVE,
+    DASH
+}
+
+public class PlayerController : MonoBehaviour
+{
+    #region parameter
+    public float Charge
+    {
+        get { return _charge; }
+        set
+        {
+            _charge = value;
+
+            if (_charge >= 1200.0f)
+            {
+                 _charge = 1200.0f;
+            }
+
+            power_bar.value = _charge / 1200.0f;
+        }
+    }
+
+    public float HP
+    {
+        get { return _hp; }
+        set
+        {
+            _hp = value;
+            hp_bar.value = _hp / 100.0f;
+
+            if (_hp <= 0.0f)
+            {
+                Debug.Log("Die");
+            }
+        }
+    }
+
+    #endregion
+
+    #region variable
+    public float _charge = 0.0f;
+    public float _hp = 100.0f;
+
+    private Animator player_anime;
+    public UIProgressBar hp_bar;
+    public UIProgressBar power_bar;
+
+    //move speed
+    public float speed = 0.0f;
+    // if you start power charge, you get low move speed
+    public float chargeSpeed;
+    // if value == 1.0f, start move anime
+    public float anime_move = 0.0f;
+
+    //dash forward
+    public Vector3 mouse;
+    public Vector2 DashWay;
+    public GameObject DashVFX;
+    public int DashFoward = 1;
+
+    private Rigidbody2D slime_Rigid;
+
+    public State_Slime state_slime = State_Slime.NONE;
+    #endregion
+
+    private void Awake()
+    {
+        slime_Rigid = GetComponent<Rigidbody2D>();
+        player_anime = GetComponent<Animator>();
+        DashVFX = transform.GetChild(0).gameObject;
+        
+        hp_bar.value = 1.0f;
+        power_bar.value = 0.0f;
+        power_bar.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    { 
+        if (slime_Rigid.velocity.magnitude <= 0.1f)
+        {
+            state_slime = State_Slime.IDLE;
+        }
+        else if (0.1f < slime_Rigid.velocity.magnitude && slime_Rigid.velocity.magnitude <= 1.0f)
+        {
+            state_slime = State_Slime.MOVE;
+        }
+        else if (1.0f < slime_Rigid.velocity.magnitude)
+        {
+            state_slime = State_Slime.DASH;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //게임상에서 마우스의 위치를 계산한다.
+        mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        DashWay = castPoint.origin-transform.position;
+
+        chargeSpeed = 1.0f;
+
+        //키 입력
+        InputKey();
+    }
+
+    void InputKey()
+    {
+        //key == Space bar
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            player_anime.SetFloat("X", anime_move);
+            anime_move = 0.0f;
+        }
+        else
+        {
+            DashVFX.SetActive(true);
+            float angle = Mathf.Atan2(DashWay.y, DashWay.x) * Mathf.Rad2Deg;
+            DashVFX.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            
+            power_bar.gameObject.SetActive(true);
+            Charge += 20.0f;
+            chargeSpeed = 2.0f;
+        }
+        // key == WASD
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed / chargeSpeed * Time.deltaTime);
+            anime_move = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed / chargeSpeed * Time.deltaTime);
+            transform.localScale = new Vector3(-1.0f, 1.0f, 0.0f);
+            DashFoward = -1;
+            anime_move = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed / chargeSpeed * Time.deltaTime);
+            anime_move = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed / chargeSpeed * Time.deltaTime);
+            transform.localScale = new Vector3(1.0f, 1.0f, 0.0f);
+            DashFoward = 1;
+            anime_move = 1.0f;
+        }
+
+        //key up Space bar
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            DashVFX.SetActive(false);
+            power_bar.gameObject.SetActive(false);
+            GetComponent<Rigidbody2D>().AddForce(DashWay.normalized * _charge, ForceMode2D.Force);
+            Charge = 0.0f;
+        }
+
+        DashVFX.transform.localScale = new Vector3(DashFoward*(_charge / 1200.0f), 0.8f, 0.0f);
+        GetComponent<Rigidbody2D>().velocity = Vector3.Slerp(GetComponent<Rigidbody2D>().velocity, Vector2.zero, 5.0f * Time.deltaTime);
+
+        //stop move
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        //}
+    }
+}
