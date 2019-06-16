4f20b6f292aeff79497ede65a907f1cfbff5ec6d
djfdmator <43840175+djfdmator@users.noreply.github.com>
2019-06-17 01:20:10 +0900

    dpdp
    Ek

--- SLIM(e)OP/Assets/Script/PlayerController.cs
+++ SLIM(e)OP/Assets/Script/PlayerController.cs
@@ -50,6 +50,7 @@ public class PlayerController : MonoBehaviour
     public float _charge = 0.0f;
     public float _hp = 100.0f;
 
+    //외부 참조 오브젝트 컴포넌트
     private Animator player_anime;
     public UIProgressBar hp_bar;
     public UIProgressBar power_bar;
@@ -60,6 +61,8 @@ public class PlayerController : MonoBehaviour
     public float chargeSpeed;
     // if value == 1.0f, start move anime
     public float anime_move = 0.0f;
+    //이동 가능 조건
+    public Vector2 moveInput;
 
     //dash forward
     public Vector3 mouse;
@@ -67,13 +70,21 @@ public class PlayerController : MonoBehaviour
     public GameObject DashVFX;
     public int DashFoward = 1;
 
-    private Rigidbody2D slime_Rigid;
+    //animator 관련 변수
+    int Walk = Animator.StringToHash("Walk");
+    int Dash = Animator.StringToHash("Dash");
 
+    //오브젝트 컴포넌트 변수 모음
+    private Rigidbody2D slime_Rigid;
+    private Animator animator;
+
+    //캐릭터의 현재 상태
     public State_Slime state_slime = State_Slime.NONE;
     #endregion
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         slime_Rigid = GetComponent<Rigidbody2D>();
         player_anime = GetComponent<Animator>();
         DashVFX = transform.GetChild(0).gameObject;
@@ -83,17 +94,50 @@ public class PlayerController : MonoBehaviour
         power_bar.gameObject.SetActive(false);
     }
 
+    private void Start()
+    {
+        StartCoroutine(Animate());
+    }
+
+    //애니메이션 작동
+    IEnumerator Animate()
+    {
+        while (true)
+        {
+            switch (state_slime)
+            {
+                case State_Slime.IDLE:
+                    animator.SetBool(Walk, false);
+                    yield return null;//new WaitForSeconds(0.2f);
+                    break;
+                case State_Slime.MOVE:
+                    animator.SetBool(Walk, true);
+                    yield return null;// new WaitForSeconds(0.2f);
+                    break;
+                case State_Slime.DASH:
+                    animator.SetTrigger(Dash);
+                    yield return null;// new WaitForSeconds(0.2f);
+                    break;
+            }
+            yield return null;
+        }
+    }
     private void Update()
-    { 
+    {
+        Slime_State();
+    }
+
+    private void Slime_State()
+    {
         if (slime_Rigid.velocity.magnitude <= 0.1f)
         {
             state_slime = State_Slime.IDLE;
         }
-        else if (0.1f < slime_Rigid.velocity.magnitude && slime_Rigid.velocity.magnitude <= 1.0f)
+        else if (0.1f < slime_Rigid.velocity.magnitude && slime_Rigid.velocity.magnitude <= 0.2f)
         {
             state_slime = State_Slime.MOVE;
         }
-        else if (1.0f < slime_Rigid.velocity.magnitude)
+        else if (0.2f < slime_Rigid.velocity.magnitude)
         {
             state_slime = State_Slime.DASH;
         }
@@ -115,63 +159,54 @@ public class PlayerController : MonoBehaviour
     void InputKey()
     {
         //key == Space bar
-        if (!Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            player_anime.SetFloat("X", anime_move);
-            anime_move = 0.0f;
-        }
-        else
-        {
+            //대쉬 방향 표시 회전각 계산
             DashVFX.SetActive(true);
             float angle = Mathf.Atan2(DashWay.y, DashWay.x) * Mathf.Rad2Deg;
             DashVFX.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            
+            DashVFX.transform.localScale = new Vector3(DashFoward * (_charge / 1200.0f), 0.8f, 0.0f);
+
+            //대쉬 파워 게이지바 
             power_bar.gameObject.SetActive(true);
             Charge += 20.0f;
             chargeSpeed = 2.0f;
         }
+
         // key == WASD
-        if (Input.GetKey(KeyCode.W))
+        if (state_slime != State_Slime.DASH)
         {
-            transform.Translate(Vector3.up * speed / chargeSpeed * Time.deltaTime);
-            anime_move = 1.0f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed / chargeSpeed * Time.deltaTime);
-            transform.localScale = new Vector3(-1.0f, 1.0f, 0.0f);
-            DashFoward = -1;
-            anime_move = 1.0f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * speed / chargeSpeed * Time.deltaTime);
-            anime_move = 1.0f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed / chargeSpeed * Time.deltaTime);
-            transform.localScale = new Vector3(1.0f, 1.0f, 0.0f);
-            DashFoward = 1;
-            anime_move = 1.0f;
+            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            slime_Rigid.velocity = moveInput.normalized * speed / chargeSpeed * Time.deltaTime;
+
+            if(Input.GetAxisRaw("Horizontal") < 0.0f)
+            {
+                transform.localScale = new Vector3(-1.0f, 1.0f, 0.0f);
+                DashFoward = -1;
+            }
+            else if(Input.GetAxisRaw("Horizontal") > 0.0f)
+            {
+                transform.localScale = new Vector3(1.0f, 1.0f, 0.0f);
+                DashFoward = 1;
+            }
         }
 
-        //key up Space bar
+        //key up Space bar - 대쉬
         if (Input.GetKeyUp(KeyCode.Space))
         {
             DashVFX.SetActive(false);
             power_bar.gameObject.SetActive(false);
-            GetComponent<Rigidbody2D>().AddForce(DashWay.normalized * _charge, ForceMode2D.Force);
+            slime_Rigid.AddForce(DashWay.normalized * _charge, ForceMode2D.Force);
             Charge = 0.0f;
         }
 
-        DashVFX.transform.localScale = new Vector3(DashFoward*(_charge / 1200.0f), 0.8f, 0.0f);
-        GetComponent<Rigidbody2D>().velocity = Vector3.Slerp(GetComponent<Rigidbody2D>().velocity, Vector2.zero, 5.0f * Time.deltaTime);
+        //이동을 천천히 멈춘다.
+        slime_Rigid.velocity = Vector3.Slerp(slime_Rigid.velocity, Vector2.zero, 5.0f * Time.deltaTime);
 
         //stop move
         //if (Input.GetKey(KeyCode.E))
         //{
-        //    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        //    slime_Rigid.velocity = Vector2.zero;
         //}
     }
 }
