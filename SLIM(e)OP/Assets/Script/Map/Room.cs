8aaf332957c2d6725efa7bc3e34176c7475c61cf
djfdmator <djfdmator@gmail.com>
2019-10-18 21:24:26 +0900

    minimap + mapCreate etc

---
+++ SLIM(e)OP/Assets/Script/Map/Room.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoomType
+{
+    Begin,
+    Normal,
+    Stair,
+    NPC,
+    Hidden
+}
+
+public enum RoomState
+{
+    DeActivate,
+    Activate,
+    Clear
+}
+
+//RoomManager 이름 수정해야함
+public class Room : MonoBehaviour
+{
+    public Vector2 gridPos;
+
+    public RoomType roomType;
+
+    public bool doorTop, doorBot, doorLeft, doorRight;
+
+    public GameObject[] door_All = new GameObject[4];
+
+    public RoomState roomState = RoomState.DeActivate;
+
+    public int enemyCount;
+
+    public RoomManager roomManager;
+
+    public int depth;
+
+    public GameObject MiniMapPos
+    {
+        get { return miniMapPos; }
+        set
+        {
+            miniMapPos = value;
+            switch(roomType)
+            {
+                case RoomType.Begin:
+                    miniMapPos.GetComponent<UISprite>().color = Color.cyan;
+                    break;
+                case RoomType.Stair:
+                    miniMapPos.GetComponent<UISprite>().color = Color.green;
+                    break;
+            }
+        }
+    }
+    private GameObject miniMapPos;
+
+    void Update()
+    {
+        enemyCount = transform.GetComponentsInChildren<Monster>().Length;
+
+        if (!roomState.Equals(RoomState.Clear))
+        {
+            if (enemyCount == 0 && roomState.Equals(RoomState.Activate))
+            {
+                IsClear();
+            }
+            else CheckPlayerPos();
+        }
+    }
+
+    void CheckPlayerPos()
+    {
+        Vector2 PlayerPos = new Vector2(roomManager.player_PosX, roomManager.player_PosY);
+        if (gridPos == PlayerPos)
+        {
+            roomState = RoomState.Activate;
+
+        }
+    }
+
+    //방이 클리어되면 작동하는 함수
+    void IsClear()
+    {
+        roomState = RoomState.Clear;
+        OpenAllDoor();
+        MiniMapPos.SetActive(true);
+        //CollectAll_Items();
+
+        //계단방의 경우 문이 열립니다.
+        if (roomType.Equals(RoomType.Stair))
+        {
+            gameObject.transform.Find("Stair").GetComponent<Stair>().IsOpen = true;
+        }
+    }
+
+    /// <summary>
+    /// [구현중] 방에 존재하는 모든 아이템을 인벤토리에 넣습니다.
+    /// 아이템 드랍이 어떻게 될 것인지 먼저 정해야한다.
+    /// </summary>
+    void CollectAll_Items()
+    {
+        GameObject[] items = GameObject.FindGameObjectsWithTag("item");
+        #region 아이템 전부 먹기
+        //구현 필요
+        #endregion
+    }
+
+    /// <summary>
+    /// 방이 클리어되면 문이 열립니다.
+    /// </summary>
+    void OpenAllDoor()
+    {
+        GameObject wall = transform.Find("wall").gameObject;
+        GameObject North = wall.transform.Find("North").gameObject;
+        GameObject South = wall.transform.Find("South").gameObject;
+        GameObject West = wall.transform.Find("West").gameObject;
+        GameObject East = wall.transform.Find("East").gameObject;
+
+        foreach (GameObject obj in door_All)
+        {
+            if (obj == null) continue;
+
+            switch (obj.name)
+            {
+                case "North":
+                    Destroy(North);
+                    break;
+                case "South":
+                    Destroy(South);
+                    break;
+                case "West":
+                    Destroy(West);
+                    break;
+                case "East":
+                    Destroy(East);
+                    break;
+            }
+
+            obj.SetActive(true);
+        }
+    }
+
+    public void SetData(Vector2 _gridPos, RoomType _roomType, RoomManager _roomManager, int _depth)
+    {
+        gridPos = _gridPos;
+        roomType = _roomType;
+        roomManager = _roomManager;
+        depth = _depth;
+        if (!_roomType.Equals(RoomType.Begin))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
