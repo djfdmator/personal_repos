87ebd2c2967d85e1367e38e80c80a5dc40c5a138
djfdmator <djfdmator@gmail.com>
2020-07-10 13:49:36 +0900

    project init

---
+++ Prj_WTH/Assets/Scripts/NPCAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnim : MonoBehaviour
+{
+    public UISprite head, body;
+
+
+    public void TestButton_Appear()
+    {
+
+    }
+
+    public void TestButton_Disappear()
+    {
+
+    }
+}
